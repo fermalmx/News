6cf8d0538bb90e33bc3ca550d1175d9342535d3a
L.Maldonado <fmt_95@outlook.com>
2020-10-02 14:07:19 -0600

    Se generaron los componentes y rutas necesarias de la aplicación.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NewsComponent } from './components/news/news.component';
+import { PostComponent } from './components/post/post.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bienvenido', component: WelcomeComponent },
+  { path: 'noticias', component: NewsComponent },
+  { path: 'entrada', component: PostComponent },
+  { path: 'perfil', component: ProfileComponent },
+  { path: '', redirectTo: '/bienvenido', pathMatch: 'full' },
+  { path: '**', component: NewsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NewsComponent } from './components/news/news.component';
+import { PostComponent } from './components/post/post.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    NewsComponent,
+    PostComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
